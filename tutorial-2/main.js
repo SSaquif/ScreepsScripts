36588cbd30448f94be3a389f4c52394a524e627f
SSaquif <ssaquif@gmail.com>
2020-03-15 17:53:02 -0400

    Readme added

--- tutorial-2/main.js
+++ tutorial-2/main.js
@@ -2,9 +2,11 @@ let Harvester = require('role.harvester');
 
 module.exports.loop = function() {
 	//console.log('roleharvester', roleHarvester.roleHarvester);
+	const harvesterArray = [];
 	for (var name in Game.creeps) {
 		var creep = Game.creeps[name];
 		let harvester = Harvester.createHarvester(creep);
+		harvesterArray.push(harvester);
 		harvester.run();
 		console.log('no error');
 	}
